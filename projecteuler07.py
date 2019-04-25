a98bad3efcf1a1b9658ea1b57ea89ae7b0e1ee44
Joël Jézéquel <47229190+djorll@users.noreply.github.com>
2019-04-25 13:58:22 +0200

    ajout énoncé et pythonisation

--- projecteuler07.py
+++ projecteuler07.py
@@ -1,26 +1,26 @@
-from time import time
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#
+# What is the 10 001st prime number?
 
+from time import time
 start = time()
 
+def est_premier(nb):
+    if nb == 1:
+        return False
+    if nb%2 == 0:
+        return False
+    for i in range(3, int(nb**0.5)+1, 2):
+        if nb%i == 0:
+            return False
+    return True
 
-def nombrepremierjusqua(x):
-    premier = [2]
-    i = 3
+rang_nbpremier = 1
+i = 1
+while rang_nbpremier != 10001:
+    i += 2
+    if est_premier(i):
+        rang_nbpremier += 1
+print(i)
 
-    while i < x:
-        verif = 1
-        for p in premier:
-            if (i % p) == 0:
-                verif = 0
-                break
-
-        if verif == 1:
-            premier.append(i)
-
-        i = i + 1
-
-    return premier
-
-
-print(nombrepremierjusqua(120000)[10000])
 print("Temps : " + str(time() - start))
