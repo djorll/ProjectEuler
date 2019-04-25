ef6de2b8a03203776560fb76e449a4742a591464
Joël Jézéquel <47229190+djorll@users.noreply.github.com>
2019-04-25 14:08:16 +0200

    ajout des énoncés

--- projecteuler09.py
+++ projecteuler09.py
@@ -1,3 +1,12 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# 
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# 
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+
 a = 1
 # comme a<b<c et a+b+c=1000 max(a)=332
 while a < 333:
